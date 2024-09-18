5a0d36e39f2c5b7bd387243838f0e2b5f4ab46a9
Gnokoua <gnokson72@gmail.com>
2024-09-18 08:29:49 +0000

    travailler sur la performance

--- src/main/java/ci/ada/fitness/repositories/PerformanceRepository.java
+++ src/main/java/ci/ada/fitness/repositories/PerformanceRepository.java
@@ -1,10 +1,14 @@
 package ci.ada.fitness.repositories;
 
 import ci.ada.fitness.models.Performance;
+import ci.ada.fitness.services.DTO.PerformanceDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 public interface PerformanceRepository extends JpaRepository<Performance, Long> {
+    Optional<Performance> findBySlug(String slug);
     // Vous pouvez ajouter des méthodes spécifiques si nécessaire
 }
