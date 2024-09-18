6677eb1e35421db7297beeadd48b0781b3ff0548
Gnokoua <gnokson72@gmail.com>
2024-09-18 09:55:34 +0000

    ajout des personalizedAdvice

--- src/main/java/ci/ada/fitness/repositories/PersonalizedAdviceRepository.java
+++ src/main/java/ci/ada/fitness/repositories/PersonalizedAdviceRepository.java
@@ -4,8 +4,11 @@ import ci.ada.fitness.models.PersonalizedAdvice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 public interface PersonalizedAdviceRepository extends JpaRepository<PersonalizedAdvice, Long> {
+    Optional<PersonalizedAdvice> findBySlug(String slug);
     // Vous pouvez ajouter des méthodes spécifiques si nécessaire
 }
 
