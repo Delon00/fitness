1b006d807b74736700439e44cc2dbeff788045d1
Delon00 <djplebob32@gmail.com>
2024-09-19 09:30:34 +0000

    Modification

--- src/main/java/ci/ada/fitness/services/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package ci.ada.fitness.services.mapper;
-
-import ci.ada.fitness.models.User;
-import ci.ada.fitness.services.DTO.UserDTO;
-
-public interface UserMapper extends EntityMapper<UserDTO, User>  {
-}
