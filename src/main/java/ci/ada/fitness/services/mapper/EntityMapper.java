c79e8632e0ca32b40f13cebfb79439493002de47
DELON <86734716+Delon00@users.noreply.github.com>
2024-09-19 08:39:28 +0000

    Delete src directory

--- src/main/java/ci/ada/fitness/services/mapper/EntityMapper.java
+++
@@ -1,7 +0,0 @@
-package ci.ada.fitness.services.mapper;
-
-public interface EntityMapper<D , E> {
-    D toDto(E entity);
-    E toEntity(D dto);
-
-}
--- src/main/java/ci/ada/fitness/services/mapper/PersonalizedAdviceMapper.java
+++
@@ -1,9 +0,0 @@
-package ci.ada.fitness.services.mapper;
-
-import ci.ada.fitness.models.PersonalizedAdvice;
-import ci.ada.fitness.services.DTO.PersonalizedAdviceDTO;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface PersonalizedAdviceMapper extends EntityMapper<PersonalizedAdviceDTO, PersonalizedAdvice>{
-}
