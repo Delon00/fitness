8a1bd4fc0ff67ca5ba188766208778f612a6f982
Nehemie <64799269+nehemie001@users.noreply.github.com>
2024-09-19 08:29:48 +0000

    test

--- src/main/java/ci/ada/fitness/models/Speciality.java
+++ src/main/java/ci/ada/fitness/models/Speciality.java
@@ -1,6 +1,11 @@
 package ci.ada.fitness.models;
 
+import jakarta.persistence.Column;
+
 import java.io.Serializable;
 
 public class Speciality extends BaseEntity implements Serializable {
+
+    @Column(name = "name", nullable = false)
+    private String name;
 }
